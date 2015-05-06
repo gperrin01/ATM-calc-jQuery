e4a4991ec4df1356bbf65001a44d42a74d19f39c
Guillaume Perrin <gperrin01@gmail.com>
2015-05-06 18:54:39 +0100

    all calcs done, moving to formatting

--- atm.js
+++ atm.js
@@ -25,21 +25,32 @@ var setupEventListerners = function() {
     $('#balance2').text('$'+SavingsAccount);
   })
 
-// withdrawal from current account
-// what if eq 0 ?
+  // withdrawal from current account
+  // what if eq 0 ?
   $('#withdraw1').on('click', function(e) {
     // withdraw amount = numCurrent, if > Current Account then take from savings
     if (numCurrent <= CurrentAccount) {
       CurrentAccount -= numCurrent;
-      $('#balance1').text('$'+CurrentAccount)
-    }
-    else if (numCurrent <= CurrentAccount + SavingsAccount) {
+      $('#balance1').text('$'+CurrentAccount);
+    } 
+    else if (numCurrent <= (CurrentAccount + SavingsAccount)) {
+      SavingsAccount = SavingsAccount - (numCurrent - CurrentAccount);
       CurrentAccount = 0;
-      SavingsAccount -= numCurrent - CurrentAccount;
+      $('#balance1').text('$'+CurrentAccount);
       $('#balance2').text('$'+SavingsAccount);
     }
-    else alert('You do not have sufficient funds to perform this operation.\n' + 'The maximum amount you can withdraw is $' + (CurrentAccount+SavingsAccount +'\n\nGet a job!!'))
+      else alert('You do not have sufficient funds to perform this operation.\n' + 'The maximum amount you can withdraw is $' + CurrentAccount+SavingsAccount +'\n\nGet a job!!')
   })
+
+  // withdrawal from savings account
+  $('#withdraw2').on('click', function(e) {
+    if (numSavings <= SavingsAccount) {
+      SavingsAccount -= numSavings;
+      $('#balance2').text('$'+SavingsAccount);
+    }
+    else alert('You do not have sufficient funds to perform this operation.\n' + 'The maximum amount you can withdraw is $' + SavingsAccount +'\n\nGet a job!!')
+  })
+
 
 }// end of event listerners declaration
 
